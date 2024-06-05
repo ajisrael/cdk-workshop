1babfef72e68207be1979233fe937817b530fa14
Alex Israels <43039187+ajisrael@users.noreply.github.com>
2024-06-05 15:00:22 -0400

    f   - created lambda construct

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -1,8 +1,14 @@
 import * as cdk from 'aws-cdk-lib';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
 
 export class CdkWorkshopStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    const hello = new lambda.Function(this, 'HelloHandler', {
+      runtime: lambda.Runtime.NODEJS_18_X,
+      code: lambda.Code.fromAsset('lambda),'), // code loaded from "lambda" directory
+      handler: 'hello.handler'
+    });
   }
 }
