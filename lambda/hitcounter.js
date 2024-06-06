04b748c44b5ff8072bdc61ebf3152e3a2a951aeb
Alex Israels <43039187+ajisrael@users.noreply.github.com>
2024-06-06 09:16:17 -0400

    b   - fixed permissions and runtime for hit counter lambda

--- lambda/hitcounter.js
+++ lambda/hitcounter.js
@@ -1,4 +1,4 @@
-const { DynamoDB, Lambda } = require('aws-sdk')
+const { DynamoDB, Lambda } = require('aws-sdk');
 
 exports.handler = async function(event) {
   console.log("request:", JSON.stringify(event, undefined, 2));
@@ -7,7 +7,7 @@ exports.handler = async function(event) {
   const dynamo = new DynamoDB();
   const lambda = new Lambda();
 
-  // update dynamodb entry for "path" with hits++
+  // update dynamodb entry for "path" with hits
   await dynamo.updateItem({
     TableName: process.env.HITS_TABLE_NAME,
     Key: { path: { S: event.path } },
